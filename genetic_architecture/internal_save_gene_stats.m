70c0d110cf8c9f13fc96f7b8392d88d2eeec640c
Or Zuk <oorzuk@gmail.com>
2017-09-11 01:03:54 +0300

    saved parameters text file

--- genetic_architecture/internal_save_gene_stats.m
+++ genetic_architecture/internal_save_gene_stats.m
@@ -12,10 +12,10 @@ function internal_save_gene_stats(GeneStruct, ExonsGeneStruct, output_data_dir, 
 save(fullfile(output_data_dir, ...
     [remove_suffix_from_file_name(remove_dir_from_file_name(gene_struct_input_file)) '_fitted_stats.mat']), '-struct', 'GeneStruct'); % save .mat file 
 
-R_header = ['Gene' exome_struct.pop_str(1:end-1) exome_struct.pop_str(1:end-1)]; % TMP! remove last one 
+R_header = ['Gene' exome_struct.pop_str(1:2) exome_struct.pop_str(1:2)]; % TMP! remove last one 
 
 % R = cell(GeneStruct.num_genes, 10); 
-R = [GeneStruct.gene_names GeneStruct.s_MLE_vec GeneStruct.alpha_MLE_vec];
+R = [GeneStruct.gene_names num2str_cell(num2cell(GeneStruct.s_MLE_vec)) num2str_cell(num2cell(GeneStruct.alpha_MLE_vec))];
 R = [R_header' R']'; 
 
 %R = [GeneStruct.gene_names GeneStruct.s_MLE_vec
@@ -24,4 +24,4 @@ R = [R_header' R']';
 %end
 
 savecellfile(R, fullfile(output_data_dir, ...
-    [remove_suffix_from_file_name(remove_dir_from_file_name(gene_struct_input_file)) '_fitted_stats.txt'])); % save .txt file
+    [remove_suffix_from_file_name(remove_dir_from_file_name(gene_struct_input_file)) '_fitted_stats.txt']), [], 1); % save .txt file
