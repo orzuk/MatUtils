9ec6745e169b02c89fd1db93207edf0f08486e1b
orzuk <oorzuk@gmail.com>
2017-10-10 17:26:22 +0300

    added path for compilation

--- genetic_architecture/pop_gen/run_parse_site_frequency_data.m
+++ genetic_architecture/pop_gen/run_parse_site_frequency_data.m
@@ -1,6 +1,9 @@
 % Master script for parsing data for site-frequency spectrum from multiple
 % datasets and plot allele-frequencies and other results
-SetPathScript; % for running as executable 
+if(isdeployed) % run as executable
+    cd('/cs/cbio/orzuk/software/Code/Matlab'); 
+    SetPathScript; % for running as executable 
+end
 Assign24MammalsGlobalConstants; AssignGeneralConstants; AssignStatsConstants; AssignRVASConstants;
 num_bins = 0:0.01:1; % bins for what?
 params = ReadParametersFile(fullfile(github_dir, 'MatUtils', 'genetic_architecture', 'data', 'ExomeParsingParameters.txt'));
