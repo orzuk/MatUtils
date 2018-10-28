aa0e6b1006d5e22ee2b3f281a0b01278d41b444a
orzuk <oorzuk@gmail.com>
2018-10-28 16:36:29 +0200

    fixed typos

--- snp_gui/calc_allele_freq.m
+++ snp_gui/calc_allele_freq.m
@@ -1,7 +1,7 @@
 % Calculate the frequency of every SNP in a given population. We also
 % calculate the pairwise statistics (Linkage Disequilibrium), and also
 % return the snp ids vector
-function [A_freq_vec pairiwse_freq_mat data_snp_ids] = calc_allele_freq(user_dir, chip_type)
+function [A_freq_vec, pairwise_freq_mat, data_snp_ids] = calc_allele_freq(user_dir, chip_type)
 
 load(fullfile(user_dir, 'display', ['all_samples_hmm_out_' chip_type '.mat']), 'genotype_mat', 'data_snp_ids');
 
@@ -12,11 +12,11 @@ genotype_mat(genotype_mat == 3) = 2;
 A_freq_vec = single(1 - mean(genotype_mat, 2) ./ 2); % get the singleton frequencies
 
 % Now get the pairwise frequencies
-pairiwse_freq_mat = zeros(4,length(A_freq_vec));
+pairwise_freq_mat = zeros(4,length(A_freq_vec));
 
-[chr, chr_locs, pres_ind] = get_chrom_locs(data_snp_ids, chip_type); % get the locations
+%[chr, chr_locs, pres_ind] = get_chrom_locs(data_snp_ids, chip_type); % get the locations
 
 % sort the SNPs according to the chromosomes, and then the locations
-[sorted_locs I J] = sort(chr_locs);
+%[sorted_locs I J] = sort(chr_locs);
 
 
--- snp_gui/compileallmexfiles.m
+++ snp_gui/compileallmexfiles.m
@@ -1,10 +1,12 @@
 % Compile all the mex file needed for the hmm_chrom program
 
+cd(fullfile(matlab_libs_root_dir, 'stats/hmp')); 
 mex TrainHMMFromDataEM.cpp hmm_chrom_funcs.cpp
 mex FindBestPathViterbi.cpp hmm_chrom_funcs.cpp
 mex SimulateSequenceFromModel.cpp hmm_chrom_funcs.cpp
 mex ComputeHMMKLDistance.cpp hmm_chrom_funcs.cpp
 mex ComputeHMMLogLikelihood.cpp hmm_chrom_funcs.cpp
 mex ReadGenotypeFile.cpp hmm_chrom_funcs.cpp hapmap_funcs.cpp
-mex MixtureOfGaussiansGivenInit.cpp hmm_chrom_funcs.cpp
-mex MixtureOfGaussiansGivenInitSingle.cpp hmm_chrom_funcs.cpp
+cd(fullfile(matlab_libs_root_dir, 'stats/mog')); 
+mex MixtureOfGaussiansGivenInit.cpp ../hmp/hmm_chrom_funcs.cpp
+mex MixtureOfGaussiansGivenInitSingle.cpp ../hmp/hmm_chrom_funcs.cpp
